22dbf62c31e010ee450025b7ab4f3f94721e5b9f
DevSnox <me.devsnox@gmail.com>
2019-02-10 23:36:05 +0100

    Hotfix added String for command

--- velocity/src/main/java/me/devsnox/pingcounter/velocity/VelocityPingCounter.kt
+++ velocity/src/main/java/me/devsnox/pingcounter/velocity/VelocityPingCounter.kt
@@ -18,32 +18,32 @@ import me.devsnox.pingcounter.core.impl.BasicPingCounter
  */
 @Suppress("unused")
 @Plugin(
-		id = "pingcounter",
-		name = "PingCounter",
-		version = "1.2-SNAPSHOT",
-		description = "PingCounter counts all incoming pings from the serverlist.",
-		authors = ["DevSnox", "Lars Artmann | LartyHD"]
+        id = "pingcounter",
+        name = "PingCounter",
+        version = "1.2-SNAPSHOT",
+        description = "PingCounter counts all incoming pings from the serverlist.",
+        authors = ["DevSnox", "Lars Artmann | LartyHD"]
 )
 class VelocityPingCounter @Inject private constructor(
-		private val commandManager: CommandManager,
-		private val eventManager: EventManager
+        private val commandManager: CommandManager,
+        private val eventManager: EventManager
 ) {
 
-	/**
-	 * @author Lars Artmann | LartyHD
-	 * Created by Lars Artmann | LartyHD on 10.02.2019 23:17.
-	 *
-	 * WARNING: This is an event. Do not call it manually!
-	 *
-	 * @param event specifies which event is intercepted. In this case it is the [ProxyInitializeEvent].
-	 *
-	 * Current Version: 1.0 (10.02.2019 - 10.02.2019)
-	 */
-	@Suppress("UNUSED_PARAMETER")
-	@Subscribe
-	fun on(event: ProxyInitializeEvent): Unit = BasicPingCounter().run {
-		commandManager.register(PingCommand(stats))
-		eventManager.register(this@VelocityPingCounter, PingListener(engine))
-	}
+    /**
+     * @author Lars Artmann | LartyHD
+     * Created by Lars Artmann | LartyHD on 10.02.2019 23:17.
+     *
+     * WARNING: This is an event. Do not call it manually!
+     *
+     * @param event specifies which event is intercepted. In this case it is the [ProxyInitializeEvent].
+     *
+     * Current Version: 1.0 (10.02.2019 - 10.02.2019)
+     */
+    @Suppress("UNUSED_PARAMETER")
+    @Subscribe
+    fun on(event: ProxyInitializeEvent): Unit = BasicPingCounter().run {
+        commandManager.register(PingCommand(stats), "pingcounter")
+        eventManager.register(this@VelocityPingCounter, PingListener(engine))
+    }
 
 }
