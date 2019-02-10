edce00a832bb845c036d6aaac4de59400e52e9b4
LartyHD <LartyHD@minewar.de>
2019-02-10 23:21:48 +0100

    Fixed the initialization problem

--- velocity/src/main/java/me/devsnox/pingcounter/velocity/VelocityPingCounter.kt
+++ velocity/src/main/java/me/devsnox/pingcounter/velocity/VelocityPingCounter.kt
@@ -1,32 +1,48 @@
 package me.devsnox.pingcounter.velocity
 
 import com.google.inject.Inject
+import com.velocitypowered.api.command.CommandManager
+import com.velocitypowered.api.event.EventManager
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
-import com.velocitypowered.api.proxy.ProxyServer
 import me.devsnox.pingcounter.core.impl.BasicPingCounter
 
 /**
  * Created by DevSnox on 12.02.18
- * Copyright (c) 2018 DevSnox
+ * Copyright (c) 2018 DevSnox | Lars Artmann | LartyHD
  * GitHub: https://github.com/DevSnox
  * Web: http://devsnox.me
  * Mail: me.devsnox@gmail.com
  * Discord: DevSnox#4884 | Skype: live:chaos3729
  */
 @Plugin(
-        id = "pingcounter",
-        name = "PingCounter",
-        version = "1.2-SNAPSHOT",
-        description = "PingCounter counts all incoming pings from the serverlist.",
-        authors = ["DevSnox"]
+		id = "pingcounter",
+		name = "PingCounter",
+		version = "1.2-SNAPSHOT",
+		description = "PingCounter counts all incoming pings from the serverlist.",
+		authors = ["DevSnox", "Lars Artmann | LartyHD"]
 )
 
-class VelocityPingCounter @Inject private constructor(proxyServer: ProxyServer) {
+class VelocityPingCounter @Inject private constructor(
+		private val commandManager: CommandManager,
+		private val eventManager: EventManager
+) {
 
-    init {
-        val pingCounter = BasicPingCounter()
+	/**
+	 * @author Lars Artmann | LartyHD
+	 * Created by Lars Artmann | LartyHD on 10.02.2019 23:17.
+	 *
+	 * WARNING: This is an event. Do not call it manually!
+	 *
+	 * @param event specifies which event is intercepted. In this case it is the [ProxyInitializeEvent].
+	 *
+	 * Current Version: 1.0 (10.02.2019 - 10.02.2019)
+	 */
+	@Subscribe
+	fun on(event: ProxyInitializeEvent): Unit = BasicPingCounter().run {
+		commandManager.register(PingCommand(stats))
+		eventManager.register(this@VelocityPingCounter, PingListener(engine))
+	}
 
-        proxyServer.commandManager.register(PingCommand(pingCounter.stats))
-        proxyServer.eventManager.register(this, PingListener(pingCounter.engine))
-    }
 }
